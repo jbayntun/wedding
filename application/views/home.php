e1c7933026a84c6ab642559ca7fecd23e0c4db58
jbayntun <j_bayntun@hotmail.com>
2015-04-07 23:46:36 -0700

    more polite front page
    Signed-off-by: jbayntun <j_bayntun@hotmail.com>

--- application/views/home.php
+++ application/views/home.php
@@ -1,3 +1,5 @@
 <h2>Welcome to <span>our wedding!</span></h2>
 
-<p>You&rsquo;re ugly.</p>
+<p>This site is currently under construction, but will soon host event information and provide 
+    a way for guests to RSVP to the Qi Wu and Jeff Bayntun wedding, which will take place on
+    September 6, 2015.</p>
